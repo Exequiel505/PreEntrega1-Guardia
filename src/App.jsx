48fcaf71979327328014803d0268a12e3ae544bc
exequielguardia@gmail.com <exequielguardia@gmail.com>
2024-05-23 11:02:34 -0300

    ProyectoFinal

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import EjemploContadorEventListener from "./components/Examples/EjemploContadorEventListener";
 import "./App.css";
-import EjemploFormulario from "./components/Examples/EjemploFormulario";
 function App() {
   return (
     <BrowserRouter>
@@ -14,8 +12,7 @@ function App() {
         <Route path="/" element = { <ItemListContainer saludo={"Bienvenidos"}/>}/>
         <Route path="/category/:idCategory" element={<ItemListContainer saludo = {"Pagina de mundo abierto"}/>}/>
         <Route path="/detail/:idProduct" element={<ItemDetailContainer/>}/>
-        {/* <Route path="/ejemplos" element ={<EjemploContadorEventListener/>}/> */}
-        <Route path="/ejemplos" element = {<EjemploFormulario/>}/>
+        
       </Routes>
     </BrowserRouter>
   );
